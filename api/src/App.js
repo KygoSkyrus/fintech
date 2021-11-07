7fb720292ed8609b856f4b0e0d7dbab8cc651175
KygoSkyrus <kygoskyrus@gmail.com>
2021-11-07 22:26:56 +0530

    <p>Symbol : {symbol}</p>             <p>name : {info["2. name"]}</p>             <p>type : {info["3. type"]}</p>             <p>Region : {info["4. region"]}</p>             <p>marketOpen : {info["5. marketOpen"]}</p>             <p>marketClose : {info["6. marketClose"]}</p>             <p>Time zone : {timeZone}</p>             <p>currency : {info["8. currency"]}</p>

--- api/src/App.js
+++ api/src/App.js
@@ -25,7 +25,9 @@ function App() {
   const [symbol, setsymbol] = useState();
   const [timeZone, settimeZone] = useState();
   const [info, setinfo] = useState();
+  const [warning, setwarning] = useState(false);
   const apiKey = "XZOG02EWJKHIHJ2Z";
+
 
   const [symbols, setsymbols] = useState([]);
 
@@ -87,7 +89,7 @@ function App() {
         console.log(data);
         if (data.Note === "Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency." || data["Error Message"] === "Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.") {
           console.log("API limit reached, try again in a minute!!!")
-
+          setwarning(true);
         } else {
           parseObjectKeys(data);
           setInfoData(symb);
@@ -125,68 +127,107 @@ function App() {
 
     const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${s}&apikey=IVKQJTLVOXVEB51T`;
 
-    fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.bestmatches);
-        if (data !== undefined) {
-          data.bestMatches.map(x => x["1. symbol"] == s ? setinfo(x) : null)
-        }
-      })
+    if (s == "") {
+      return null;
+    } else {
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data.bestmatches);
+          if (data !== undefined) {
+            data.bestMatches.map(x => x["1. symbol"] == s ? setinfo(x) : null)
+          }
+        })
+    }
   }
 
 
   return (
-    <div className="app">
+    <>
+      <div className="app">
 
-      <div className="left">
-        <h1 className="mb-4">ROCK STOCK</h1>
+        <div className="left">
+          <h1 className="mb-4">ROCK STOCK</h1>
 
-        <div className="mb-3">
-          <label htmlFor="browser">Enter stock symbol:</label>
-          <input type="text" id="symbol" name="symbol" list="symbols" onInput={() => getSymbol()} placeholder="Enter symbol" autoComplete="off" required className="form-control" />
+            <div className="mb-3">
+              <label htmlFor="browser">Enter stock symbol:</label>
+              <input type="text" id="symbol" name="symbol" list="symbols" onInput={() => getSymbol()} placeholder="for e.g. 'TSLA'" autoComplete="off" required className="form-control" />
 
-          <datalist id="symbols">
-            {symbols === undefined ? null :
-              symbols.map(x => <option value={x["1. symbol"]} key={x["1. symbol"]} >{x["2. name"]}</option>)}
-          </datalist>
+              <datalist id="symbols">
+                {symbols === undefined ? null :
+                  symbols.map(x => <option value={x["1. symbol"]} key={x["1. symbol"]} >{x["2. name"]}</option>)}
+              </datalist>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="ts">Timeseries : </label>
+              <select name="select" id="select" className="form-select" placeholder="--Choose a timeseries--" required>
+                <option value="">--Choose a timeseries--</option>
+                <option value="DAILY">Daily</option>
+                <option value="WEEKLY">Weekly</option>
+                <option value="MONTHLY">Monthly</option>
+              </select>
+            </div>
+
+            <button onClick={() => getData()} type="submit" className="btn btn-outline-secondary w-100 text-dark ">SHOW</button>
+
+
+          {info ?
+            <div className="info">
+              <table className="">
+                <tr>
+                  <td>Name</td>
+                  <td>{info["2. name"]}</td>
+                </tr>
+                <tr>
+                  <td>Type</td>
+                  <td>{info["3. type"]}</td>
+                </tr>
+                <tr>
+                  <td>Region</td>
+                  <td>{info["4. region"]}</td>
+                </tr>
+                <tr>
+                  <td>Marketopen</td>
+                  <td>{info["5. marketOpen"]}</td>
+                </tr>
+                <tr>
+                  <td>Marketclose</td>
+                  <td>{info["6. marketClose"]}</td>
+                </tr>
+                <tr>
+                  <td>Timezone</td>
+                  <td>{timeZone}</td>
+                </tr>
+                <tr>
+                  <td>Currency</td>
+                  <td>{info["8. currency"]}</td>
+                </tr>
+              </table>
+            </div>
+            : null
+          }
+
         </div>
 
-        <div className="mb-3">
-          <label htmlFor="ts">Timeseries : </label>
-          <select name="select" id="select" className="form-select">
-            <option value="">--Choose a timeseries--</option>
-            <option value="DAILY">Daily</option>
-            <option value="WEEKLY">Weekly</option>
-            <option value="MONTHLY">Monthly</option>
-          </select>
+        <div id="line"></div>
+
+        <div className="chart">
+          <Graph data={data} symbol={symbol} />
         </div>
-
-        <button onClick={() => getData()} type="submit" value="submit" className="btn btn-outline-secondary w-100 text-dark ">SHOW</button>
-
-
-        {info ?
-          <div className="lh-1">
-            <p>Symbol : {symbol}</p>
-            <p>name : {info["2. name"]}</p>
-            <p>type : {info["3. type"]}</p>
-            <p>Region : {info["4. region"]}</p>
-            <p>marketOpen : {info["5. marketOpen"]}</p>
-            <p>marketClose : {info["6. marketClose"]}</p>
-            <p>Time zone : {timeZone}</p>
-            <p>currency : {info["8. currency"]}</p>
-          </div>
-          : null
-        }
-
       </div>
 
-      <div className="chart">
-        <Graph data={data} symbol={symbol} />
-      </div>
-    </div>
+      {warning ?
+        <div className="alert alert-danger alert-dismissible fade show error" role="alert">
+          <i class="fa fa-exclamation-triangle"></i>
+          &nbsp; <strong>API limit reached!!! </strong> Try again in a minute
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        : null
+      }
+    </>
   );
 }
 
